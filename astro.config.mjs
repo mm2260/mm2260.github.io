54b8c3ed1f22c938dc74113dea4b2976f4c844e9
Mohammed Mehboob <mm2260@g.rit.edu>
2022-12-08 23:10:44 -0500

    updated the ai-and-creativity post

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import katex from 'astro-katex';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), katex(), mdx()],
+  integrations: [tailwind(), katex(), mdx(), image()],
   site: 'https://mdmeh.com'
 });

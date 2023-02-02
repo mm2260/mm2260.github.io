13acf933efed7ff27cbead278189da1d6c4bfcd8
Mohammed Mehboob <mm2260@g.rit.edu>
2023-02-02 01:25:39 -0500

    Added math support + posts redo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import katex from 'astro-katex';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
@@ -11,6 +10,6 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), katex(), mdx(), image()],
-  site: 'https://mdmeh.com'
+  integrations: [tailwind(), mdx(), image()],
+  site: 'https://mdmeh.com',
 });
